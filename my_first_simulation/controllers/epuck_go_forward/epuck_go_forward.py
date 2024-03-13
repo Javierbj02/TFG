87bc5dc41feba87cf4085197859aacf67573985e
JAVIER BALLESTEROS JEREZ <Javier.Ballesteros4@alu.uclm.es>
2024-03-13 13:38:56 +0100

    feat: extend the controller to Speed Control

--- my_first_simulation/controllers/epuck_go_forward/epuck_go_forward.py
+++ my_first_simulation/controllers/epuck_go_forward/epuck_go_forward.py
@@ -2,7 +2,11 @@
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
-from controller import Robot
+from controller import Robot, Motor
+
+TIME_STEP = 64
+
+MAX_SPEED = 6.28
 
 # create the Robot instance.
 robot = Robot()
@@ -16,9 +20,21 @@ timestep = int(robot.getBasicTimeStep())
 #  ds = robot.getDevice('dsname')
 #  ds.enable(timestep)
 
+# Get the motor devices
+leftMotor = robot.getDevice('left wheel motor')
+rightMotor = robot.getDevice('right wheel motor')
+
+# Set the target position of the motors
+leftMotor.setPosition(float('inf'))
+rightMotor.setPosition(float('inf'))
+
+leftMotor.setVelocity(0.1 * MAX_SPEED)
+rightMotor.setVelocity(0.1 * MAX_SPEED)
+
+
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
+while robot.step(TIME_STEP) != -1:
     # Read the sensors:
     # Enter here functions to read sensor data, like:
     #  val = ds.getValue()
