d57f4820585a13cf217b17009002471c954979f0
Javierbj02 <javier.ballesteros4@alu.uclm.es>
2024-05-27 09:30:46 +0200

    feat: add URA 16 controller

---
+++ my_first_simulation/controllers/model_controller_ura16/model_controller_ura16.py
@@ -0,0 +1,12 @@
+from controller import Supervisor
+from robot_controller import RobotController
+
+main_root = "C:/Users/javi2/Desktop/TFG - Webots/TFG/"
+scenario = "URA"
+num_antennas = "16"
+robot_controller = RobotController(Supervisor(), scenario, num_antennas)
+
+print(robot_controller.get_real_position())
+print("------------------------------------")
+
+robot_controller.getReading(main_root, robot_controller.get_real_position())
