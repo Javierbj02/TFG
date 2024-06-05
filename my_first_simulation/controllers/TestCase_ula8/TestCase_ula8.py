de218a62db0b500a9617071ba6e7221aa9493cf0
Javierbj02 <javier.ballesteros4@alu.uclm.es>
2024-06-05 19:54:57 +0200

    feat: add first steps of the test cases

---
+++ my_first_simulation/controllers/TestCase_ula8/TestCase_ula8.py
@@ -0,0 +1,65 @@
+"""TestCase_ula8 controller."""
+
+from controller import Supervisor
+from robot_controller import RobotController
+import numpy as np
+import pandas as pd
+
+main_root = "C:/Users/javi2/Desktop/TFG - Webots/TFG/"
+scenario = "ULA"
+num_antennas = "8"
+ruido = "Ruido bajo"
+robot_controller = RobotController(Supervisor(), scenario, num_antennas)
+
+# robot_controller.df = robot_controller.load_dataset(main_root, scenario, num_antennas)
+robot_controller.df = robot_controller.load_dataset_testCases(main_root, scenario, num_antennas, ruido)
+
+print(robot_controller.get_real_position())
+print("------------------------------------")
+
+TIME_STEP = 16
+MAX_SPEED = 2
+
+
+leftSpeed = 0.5 * MAX_SPEED
+rightSpeed = 0.5 * MAX_SPEED
+
+robot_controller.left_motor.setVelocity(leftSpeed)
+robot_controller.right_motor.setVelocity(rightSpeed)
+
+pos_array = []
+
+while robot_controller.step(TIME_STEP) != -1:
+
+    
+    print(" ")
+    print("-- -- -- --")
+    results = robot_controller.getReading2(main_root, robot_controller.get_real_position())
+    print("-- -- -- --")
+    print(" ")
+
+    if results is not None:
+        new_row = {
+            "RoundedX": results[1][0],
+            "RoundedY": results[1][1],
+            "PredictedX": results[2][0],
+            "PredictedY": results[2][1]
+        }
+
+        pos_array.append(new_row)
+
+    robot_controller.step(TIME_STEP * 10)
+
+
+    if robot_controller.getTime() > 70.0:
+        robot_controller.left_motor.setVelocity(0.0)
+        robot_controller.right_motor.setVelocity(0.0)
+        break
+
+
+print("Exiting...")
+df_positions = pd.DataFrame(pos_array)
+
+output_path = main_root + "Test Cases/Results/" + ruido + "/predicciones_" + scenario + num_antennas + ".csv"
+df_positions.to_csv(output_path, index=False)
+
---
+++ my_first_simulation/controllers/get_position_online/get_position_online.py
@@ -0,0 +1,75 @@
+"""TestCase_ula8 controller."""
+
+from controller import Supervisor
+from robot_controller import RobotController
+import numpy as np
+
+main_root = "C:/Users/javi2/Desktop/TFG - Webots/TFG/"
+scenario = "ULA"
+num_antennas = "8"
+robot_controller = RobotController(Supervisor(), scenario, num_antennas)
+
+# robot_controller.df = robot_controller.load_dataset(main_root, scenario, num_antennas)
+
+print(robot_controller.get_real_position())
+print("------------------------------------")
+
+# robot_controller.getReading(main_root, robot_controller.get_real_position())
+
+# get the time step of the current world.
+TIME_STEP = 16
+MAX_SPEED = 2
+
+
+# You should insert a getDevice-like function in order to get the
+# instance of a device of the robot. Something like:
+#  motor = robot.getDevice('motorname')
+#  ds = robot.getDevice('dsname')
+#  ds.enable(timestep)
+
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+
+leftSpeed = 0.5 * MAX_SPEED
+rightSpeed = 0.5 * MAX_SPEED
+
+robot_controller.left_motor.setVelocity(leftSpeed)
+robot_controller.right_motor.setVelocity(rightSpeed)
+
+# df = robot_controller.load_dataset(main_root, scenario, num_antennas)
+# ! Probar pasando df como parámetro al método getReading2
+# ! Probar también poniendolo como atributo de la clase RobotController
+
+position_array = []
+
+while robot_controller.step(TIME_STEP) != -1:
+
+    # robot_controller.left_motor.setVelocity(0)
+    # robot_controller.right_motor.setVelocity(0)
+    
+    print(" ")
+    print("-- -- -- --")
+    # posicion_estimada = robot_controller.getReading2(main_root, robot_controller.get_real_position())
+    # Quiero ir leyendo la posición actual del robot, y guardar cada lectura en un .npy
+    pos = robot_controller.get_real_position()
+    position_array.append(pos)
+    print(pos)
+    print("-- -- -- --")
+    print(" ")
+
+    # robot_controller.left_motor.setVelocity(leftSpeed)
+    # robot_controller.right_motor.setVelocity(rightSpeed)
+
+    robot_controller.step(TIME_STEP * 10)
+
+
+    if robot_controller.getTime() > 70.0:
+        robot_controller.left_motor.setVelocity(0.0)
+        robot_controller.right_motor.setVelocity(0.0)
+        break
+
+
+# # Enter here exit cleanup code.
+print("Exiting...")
+np.save(main_root + "Test Cases/Online positions/posiciones_reales_2.npy", position_array)
+# print(robot_controller.get_real_position())
